c927ff162b938d2b446cb2e10b18e00a49734d4a
Saran-Kathiravan <160735816+Saran-Kathiravan@users.noreply.github.com>
2024-03-23 02:13:41 +0530

    Updated functionality for blank location

--- weather.py
+++ weather.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from pprint import pprint
 import requests
 import os
+import geocoder
+
 
 load_dotenv()
 
@@ -12,10 +14,26 @@ def get_current_weather(city):
 
     return weather_data
 
+def get_current_city():
+    # Get the device's current location coordinates (latitude and longitude)
+    location = geocoder.ip('me')
+
+    # Check if location retrieval was successful
+    if location.ok:
+        # Get the city name using reverse geocoding
+        city = location.city
+        print("Current city:", city)
+    else:
+        print("Chennai")
+
+
 if __name__ == "__main__":
     print("\nGet Current weather Conditions\n")
 
     city=input("\nPlease enter a city name: ")
+
+    if not bool(city.strip()):
+        city=get_current_city()
 
     weather_data=get_current_weather(city)
     
